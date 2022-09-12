bb842242420400e299aa861b9a750ad465323318
noormesslmani <noor.messlmani@gmail.com>
2022-09-12 23:14:31 +0300

    Added a validation for contact info

---
+++ script.js
@@ -0,0 +1,46 @@
+const fullName = document.getElementById('name')
+const email = document.getElementById('email')
+const number = document.getElementById('number')
+const message = document.getElementById('message')
+const sendButton = document.getElementById('send-btn')
+const errorMsg= document.getElementById('error-msg')
+const inputInfo=[fullName, email, number,message]
+
+console.log('Hello')
+sendButton.onclick=()=>{console.log('Hello')}
+
+ sendButton.onclick= function validate(inputInfo){
+    if (validEmail(inputInfo) && validName(inputInfo) && validNum(inputInfo) && validMsg(inputInfo)){
+        errorMsg.textContent=''
+    }
+    else{
+        errorMsg.textContent='The following are invalid:'
+        if(! validEmail(inputInfo)){errorMsg.textContent+=' email '}
+        if(! validName(inputInfo)){errorMsg.textContent+=' name '}
+        if(! validNum(inputInfo)){errorMsg.textContent+=' number '}
+        if(! validMsg(inputInfo)){errorMsg.textContent+=' message '}
+    }
+}
+
+function validEmail(inputInfo){
+    //let valid=''
+    let valid=/^([a-zA-Z0-9._~-]{3,})+@([a-zA-Z0-9-]{3,})+((?:\.[a-zA-Z0-9-]+){2,})*$/
+    return (valid.test(email.value))
+    //return (false)
+}
+function validName(inputInfo){
+    return(fullName.value.trim().length>5)
+}
+function validNum(inputInfo){
+    if (number.value.substring(0,3)=="+961"){
+        if(number.value.substring(4)==3){
+            return(number.value.length==11)
+        }
+        else{
+            return(number.value.length==12)
+        }
+    }
+}
+function validMsg(inputInfo){
+    return (message.value.length>=100)
+}
