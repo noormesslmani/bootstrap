9bbf68751af05e8715c924571d7cad96a072a2ce
noormesslmani <noor.messlmani@gmail.com>
2022-09-14 00:24:28 +0300

    created a php file to rows to contact table in the database

---
+++ addcontact.php
@@ -0,0 +1,17 @@
+<?php
+include("connectioncontact.php");
+
+$fullname= $_POST["fullname"];
+$email= $_POST["email"];
+$phonenumber= $_POST["phonenumber"];
+$messages= $_POST["messages"];
+
+$query = $mysqli->prepare("INSERT INTO `contact-me`(fullname,email,phonenumber,messages) VALUE (?,?,?,?)");
+$query->bind_param("ssss", $fullname, $email,$phonenumber,$messages);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+?>
