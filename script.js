90feed4aa1356c1222767ed064980edf5c97908a
noormesslmani <noor.messlmani@gmail.com>
2022-09-14 11:18:29 +0300

    commented the js code

--- script.js
+++ script.js
@@ -11,28 +11,35 @@ const body= document.getElementById('body')
 const showMessages= document.getElementById('show-messages')
 var inputInfo=[fullName, email, number,message]
 
+
+//change screen view on tablet icon click
 tablet.onclick= function (){
     document.body.style.paddingLeft='200px'
     document.body.style.paddingRight='200px'
 }
 
+//change screen view on mobile icon click
 mobile.onclick= function (){
     document.body.style.paddingLeft='400px'
     document.body.style.paddingRight='400px'
 }
+
+//change screen view on desktop icon click
 desktop.onclick= function (){
     document.body.style.paddingLeft='0px'
     document.body.style.paddingRight='0px'
 }
 
 
+//send input to databases on button click
 sendButton.onclick= function validate(inputInfo){
-    console.log('Hello')
-
+    
+    //check if the input field are valid
     if (validEmail(inputInfo) && validName(inputInfo) && validNum(inputInfo) && validMsg(inputInfo)){
         errorMsg.textContent=''
         let url
         url='http://localhost/bootstrap/addcontact.php/'
+        //send the data to the api 
         fetch(url,{
             method: 'POST',
             body: new URLSearchParams({"fullname":fullName.value, "email":email.value, "phonenumber":number.value, "messages": message.value }),
@@ -40,6 +47,7 @@ sendButton.onclick= function validate(inputInfo){
         window.location.reload()
     }
     else{
+        //display errror message if some input is invalid
         errorMsg.textContent='The following are invalid:'
         if(! validEmail(inputInfo)){errorMsg.textContent+=' email '}
         if(! validName(inputInfo)){errorMsg.textContent+=' name '}
@@ -48,32 +56,37 @@ sendButton.onclick= function validate(inputInfo){
     }
 }   
 
+//check if email is valid
 function validEmail(inputInfo){
     let valid =/^([a-z0-9]{3,})+[@]+([a-z]{2,})+[.]+([a-z]{2,})*$/
     return (valid.test(email.value))
 }
+//check if he name is valid
 function validName(inputInfo){
     return(fullName.value.trim().length>5)
 }
+//check if phone number is valid
 function validNum(inputInfo){
     let valid1= /^[+9613]+([0-9]{6})*$/
     let valid2= /^[+961]+([0-9]{8})*$/
     return (valid1.test(number.value) || valid2.test(number.value) )
 }
+//check if message is valid
 function validMsg(inputInfo){
     return (message.value.length>=100)
 }
 
-
+//display all messages in databases on window load event
 window.onload = async function displayMsg(){
     let url
     url='http://localhost/bootstrap/getmessages.php/'
     const response = await fetch (url)
     const data = await response.json()
-    for (let i=0;i<data.length;i++){
-        let para = document.createElement("p")
+    for (let i=0;i<data.length;i++){ //loop over the data
+        let para = document.createElement("p") //create a new p to display a message
         para.setAttribute('class','all-messages')
         para.innerText = data[i].messages;
         showMessages.appendChild(para);
     }
 }
+
